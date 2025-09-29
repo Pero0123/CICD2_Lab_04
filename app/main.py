55ccf1bf10f179a54a82a8ce162982e72c3903d1
Pero <g00422221@atu.ie>
2025-09-29 17:09:59 +0100

    added tests and ci to project

--- app/main.py
+++ app/main.py
@@ -27,19 +27,22 @@ def add_user(user: User):
     users.append(user)
     return user
 
-@app.put("/api/users")
-def update_user(user_id: int, user: User):
+
+@app.put("/api/users/", status_code=status.HTTP_201_CREATED)
+def update_user(user: User):
     for u in users:
-        if (u.user_id == user.user_id):
-            users[users.index(u)] = user
+        if(u.user_id == user.user_id):
+            users[users.index(u)] = user 
             return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
-@app.delete("/api/users")
-def get_user(user_id: int):
+
+@app.delete("/api/users/{user_id}", status_code = 204)
+def delete_user(user_id: int):
     for u in users:
         if u.user_id == user_id:
             users.remove(u)
+            return
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")        
 
 
